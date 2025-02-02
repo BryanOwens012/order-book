308dd4201814b2c448327bf29d4110f93b6ea411
Bryan Owens <bryanowens012@gmail.com>
2025-02-02 01:21:33 -0500

    Add methods to top level

--- stock_exchange.py
+++ stock_exchange.py
@@ -3,8 +3,9 @@ Stock exchange
 """
 
 from dataclasses import dataclass
+from typing import Optional
 
-from order import LimitOrder, MarketOrder
+from order import LimitOrder, MarketOrder, OrderDirection
 from order_book import OrderBook
 
 
@@ -45,20 +46,48 @@ class StockExchange:
 
         return self.order_books[ticker]
 
-    def submit_market_order(self, market_order: MarketOrder):
+    def submit_market_order(self, market_order: MarketOrder) -> MarketOrder:
         """
         Submit and execute (fill, if possible) a market order
         """
 
         ticker = market_order.ticker
 
-        self.get_or_create_order_book(ticker).submit_market_order(market_order)
+        return self.get_or_create_order_book(ticker).submit_market_order(market_order)
 
-    def submit_limit_order(self, limit_order: LimitOrder):
+    def submit_limit_order(self, limit_order: LimitOrder) -> LimitOrder:
         """
         Submit a limit order
         """
 
         ticker = limit_order.ticker
 
-        self.get_or_create_order_book(ticker).submit_limit_order(limit_order)
+        return self.get_or_create_order_book(ticker).submit_limit_order(limit_order)
+
+    def cancel_limit_order(
+        self, ticker: str, direction: OrderDirection, price: float, order_id: str
+    ) -> LimitOrder:
+        """
+        Cancel a limit order
+        """
+
+        return self.get_or_create_order_book(ticker).cancel_limit_order(
+            direction, price, order_id
+        )
+
+    def update_limit_order(
+        self,
+        ticker: str,
+        direction: OrderDirection,
+        price: float,
+        order_id: str,
+        new_quantity: Optional[int],
+        new_price: Optional[float],
+    ) -> LimitOrder:
+        """
+        Update a limit order
+        """
+
+        return self.get_or_create_order_book(ticker).update_limit_order(
+            direction, price, order_id, new_quantity, new_price
+        )
